524fe7228e5f9be375b0ef63975c1effb596197d
Tianyao Wang <tianyaohasadream@gmail.com>
2020-05-16 14:54:40 -0500

    Add files via upload

---
+++ 3-file_copying.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* copy input to output; 1st version */
+main()
+{
+	int c;
+	/* Exercise 1-7 Write a program to print the value of EOF */
+	printf("%d",EOF);	
+	while ((c = getchar()) != EOF) {
+		/* Exercise 1-6 Verify that the expression getchar() != EOF is 0 or 1 */
+		printf("%d",c!=EOF);
+		putchar(c);
+	}
+	printf("%d",c!=EOF);
+}
