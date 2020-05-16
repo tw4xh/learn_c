d4533a493b84ae395da4fc8b1e8fa024aaf99fd2
Tianyao Wang <tianyaohasadream@gmail.com>
2020-05-16 01:29:56 -0500

    Add files via upload
    The code to convert Fahrenheit to Celsius

---
+++ 2_Fahrenheit_Celsius_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+	
+	lower = 0; /* lower limit of temperature table */
+	upper = 200; /* upper limit */
+	step = 20; /* step size */
+	
+	fahr = lower;
+	/* Exercise 1-3 Modify the temperature conversion program to print a heading above the table */
+	printf("Fahrenheit Celsius Table\n");
+	printf("Fahr\tCelsius\n");
+	while (fahr <= upper) {
+		celsius = 5.0 * (fahr-32)/9.0;
+		printf("%4.0f\t%7.1f\n", fahr, celsius);
+		fahr = fahr + step;
+}
+}
