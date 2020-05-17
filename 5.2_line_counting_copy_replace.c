46aa1a988460c19aa062dd99f6c72fb1ce8af161
Tianyao Wang <tianyaohasadream@gmail.com>
2020-05-17 02:30:21 -0500

    Update 5.2_line_counting_copy_replace.c

--- 5.2_line_counting_copy_replace.c
+++ 5.2_line_counting_copy_replace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 /* count lines in input */
+/* Exercise 1-9. Write a program to copy its input to its output, 
+replacing each string of one or more blanks by a single blank. */
 main()
 {
 	int c, n_space;
